89e1348ccd90b2b5bfe2d8bdfaa5dadac89ce3a7
Fllask <vallatga@gmail.com>
2024-05-30 11:41:30 +0200

    initial commit

---
+++ algorithms.py
@@ -0,0 +1,253 @@
+"""Different algorithms related to gradient descent used to optimize the actions on different kind of games
+   AUTHORS: Gabriel Vallat
+"""
+import torch as th
+import numpy as np
+
+def IPGA_stage_game(game,init_pol, iter=10000,lr=None):
+    pol = th.clone(init_pol)
+    flag=False
+    pol.requires_grad=True
+    lr = lr or 1/(game.na*(game.n-1))
+    opt = th.optim.SGD([pol], lr=lr,maximize=True)
+    
+    best_pol = th.tensor(np.unravel_index(th.argmax(game.r).cpu(),game.r.shape),device=game.device)
+    batched = pol.dim()!=2
+    r_prev = 0
+    #print(f"Result:{th.abs(pol-th.nn.functional.one_hot(best_pol,num_classes=game.na)).mean()}",end="\r")
+    for i in range(iter):
+        opt.zero_grad()
+        if batched:
+            r = game.solve_stoc_pol_batch(pol).sum()
+        else:
+            r = game.solve_stoc_pol(pol)
+        if r_prev > r:
+            print("Warning: learning rate to large with respect to the smoothness")
+        r.backward()
+        opt.step()
+        #project back into the simplex
+        pol.data=project_simplex_batch(pol)
+        #print(f"Result:{th.abs(pol-th.nn.functional.one_hot(best_pol,num_classes=game.na)).mean()}",end="\r")
+        #print(f"{r=}",end="\r")
+        m,argm = pol.max(-1)
+        #early stops if the policy is in the corner of the simplex for two steps in a row
+        if (m ==1).all():
+            if flag:
+                break
+            flag=True
+        else:
+            flag=False
+            
+    return pol
+
+def IPGA_dynamic_game_oracle(game,init_pol,s_0=None, iter=1000):
+    pol = th.clone(init_pol)
+    pol.requires_grad=True
+    opt = th.optim.SGD([pol], lr=0.05,maximize=True)
+    #print(f"Result:{th.abs(pol-th.nn.functional.one_hot(best_pol,num_classes=game.na)).mean()}",end="\r")
+    for i in range(iter):
+        opt.zero_grad()
+        V = game.play_game(pol,s_0)
+        V.mean().backward()
+        opt.step()
+        #project back into the simplex
+        pol.data = project_simplex_batch(pol)
+        print(f"{V=}",end="\r")
+
+        m,argm = pol.max(-1)
+        if (m ==1).all():
+            break
+    return pol,V
+def PGA_dynamic_game_oracle(game,init_pol,s_0=0, iter=100000):
+    pol = th.clone(init_pol)
+    pol_central = th.ones([game.ns]+[game.na]*game.n,device=game.device)
+    for p in range(game.n):
+        shape = th.ones(game.n+1,dtype=int)
+        shape[0] = game.ns
+        shape[p+1]=game.na
+        pol_central*=pol[p].reshape(tuple(shape))
+
+    pol_central = pol_central.flatten(1)
+    pol_central.requires_grad=True
+    opt = th.optim.SGD([pol_central], lr=0.1,maximize=True)
+    #print(f"Result:{th.abs(pol-th.nn.functional.one_hot(best_pol,num_classes=game.na)).mean()}",end="\r")
+    for i in range(iter):
+        opt.zero_grad()
+        V = game.play_game_CCE(pol_central,s_0=s_0)
+        V.mean().backward()
+        opt.step()
+        #project back into the simplex
+        pol_central.data = project_simplex_batch(pol_central)
+        print(f"{V=}",end="\r")
+        m,argm = pol_central.max(-1)
+        if (m ==1).all():
+            break
+    end_pol = np.array(np.unravel_index(argm.cpu().numpy(),[game.na]*game.n)).T
+    pol = th.nn.functional.one_hot(th.tensor(end_pol,device=game.device).T,num_classes=game.na)
+    return pol,V
+def SIPGA_dynamic_game_play(game,init_pol,s_0=0, iter=100000,stable_target=False,optimistic_V=False,share_pol=False):
+    """Can be seen as a directly parametrized actor-critic"""
+    pol = th.clone(init_pol)
+    if optimistic_V:
+        V = 1/(1-game.gamma)*th.ones(game.ns,device=game.device)
+    else:
+        V = th.zeros(game.ns,device=game.device)
+    V.requires_grad=True
+    #the depedence on the policy is linear, so no optimizer is required
+    #pol.requires_grad=True
+    lr_pol = 0.01
+    #opt_pol = th.optim.SGD([pol], lr=0.01,maximize=True)
+    opt_V = th.optim.SGD([V], lr=0.01,maximize=False)
+    ns = s_0
+    for i in range(iter):
+        opt_V.zero_grad()
+
+        s = ns
+        actions = [game.random_np.choice(np.arange(game.na),p=pol[p,s].detach().cpu().numpy()) for p in range(game.n)]
+        
+        ns,r = game.play_step(s,actions)
+        if stable_target:
+            V_s=  r+game.gamma*V[ns].detach()#detach to not go twice through the graph
+        else:
+            V_s=  r+game.gamma*V[ns]
+
+        action_ar = np.array(actions)
+
+        if share_pol:
+            #should not be used: the pol is already embedded in V
+            allp = th.arange(game.n)
+            for p in range(game.n):
+                notp = th.cat([allp[:0],allp[1:]]) 
+                polnotp = pol[notp,s*th.ones(game.n-1,dtype=int),action_ar[notp]]
+                pol[p,s,action_ar[p]] += lr_pol*V_s*polnotp.prod()
+        else:
+            pol[th.arange(game.n),s*th.ones(game.n,dtype=int),action_ar] += lr_pol*V_s
+        err = th.square(V_s-V[s])
+
+        err.backward()
+        opt_V.step()
+        
+        #project back into the simplex
+        pol.data = project_simplex_batch(pol)
+        print(f"{V=}",end="\r")
+
+        m,argm = pol.max(2)
+        if (m ==1).all():
+            break
+    return pol,V
+def SPGA_dynamic_game_play(game,init_pol,s_0=0, iter=100000,stable_target=True,optimistic_V=False):
+    """Find a centralized solution (a coarse correlated equilibrium)"""
+    pol = th.clone(init_pol)
+    pol_central = th.ones([game.ns]+[game.na]*game.n,device=game.device)
+    for p in range(game.n):
+        shape = th.ones(game.n+1,dtype=int)
+        shape[0] = game.ns
+        shape[p+1]=game.na
+        pol_central*=pol[p].reshape(tuple(shape))
+
+    pol_central = pol_central.flatten(1)
+    if optimistic_V:
+        V = 1/(1-game.gamma)*th.ones(game.ns,device=game.device)
+    else:
+        V = th.zeros(game.ns,device=game.device)
+    V.requires_grad=True
+    #the depedence on the policy is linear, so no optimizer is required
+    #pol.requires_grad=True
+    lr_pol = 0.01
+    #opt_pol = th.optim.SGD([pol], lr=0.01,maximize=True)
+    opt_V = th.optim.SGD([V], lr=0.01,maximize=False)
+    ns = s_0
+    for i in range(iter):
+        opt_V.zero_grad()
+
+        s = ns
+        actions_id = game.random_np.choice(np.arange(pol_central[s].numel()),p=pol_central[s].cpu().numpy())
+        actions = np.unravel_index(actions_id,[game.na]*game.n)
+        ns,r = game.play_step(s,actions)
+        if stable_target:
+            V_s=  r+game.gamma*V[ns].detach()#detach to not go twice through the graph
+        else:
+            V_s=  r+game.gamma*V[ns]
+
+        action_ar = np.array(actions)
+
+        
+        pol_central[s,actions_id]  += lr_pol*V_s
+
+        err = th.square(V_s-V[s])
+
+        err.backward()
+        opt_V.step()
+        
+        #project back into the simplex
+        pol_central.data[s] = project_simplex_batch(pol_central[s])
+        print(f"{V=}",end="\r")
+        m,argm = pol_central.max(1)
+        if (m ==1).all():
+            break
+    #note that we go to a greedy policy at the end, as we cannot necessarly convert the probability over the joint policies to the decentralized ones
+    end_pol = np.array(np.unravel_index(argm.cpu().numpy(),[game.na]*game.n)).T
+    pol = th.nn.functional.one_hot(th.tensor(end_pol,device=game.device).T,num_classes=game.na)
+    return pol,V
+def project_simplex(x,tol=1e-6,max_steps=1000000):
+    mu = 0
+    pmu = 1
+    #use Newton method on h to find mu
+    s=0
+    while abs(mu-pmu)>tol and s<max_steps:
+        s+=1
+        pmu=mu
+        mu = mu + (th.clip(x-mu,0).sum()-1)/((x-mu)>0).sum()
+    proj_x = th.clip(x-mu,0) 
+    if s==max_steps:
+        print("Warning: the projection did not converge")
+    return proj_x
+def project_simplex_batch(x,tol=1e-6,max_steps=100):
+    mu = th.zeros(x.shape[:-1],device=x.device).unsqueeze(-1)
+    pmu = th.ones(x.shape[:-1],device=x.device).unsqueeze(-1)
+    #use Newton method on h to find mu
+    s=0
+    
+    while (mu-pmu).abs().max()>tol and s<max_steps:
+        s+=1
+        pmu=mu
+        mu = mu + (th.clip(x-mu,0).sum(-1,keepdim=True)-1)/((x-mu)>0).sum(-1,keepdim=True)
+    if s==max_steps:
+        print("Warning: the projection did not converge")
+    proj_x = th.clip(x-mu,0) 
+    return proj_x
+def test_stage():
+    import game_definition
+    game = game_definition.StageGame(3,10,coupling_w=0.5,seed=231242351,device="cuda")
+    init_pol = th.rand(100,game.n,game.na,device=game.device,generator=game.gen)
+    init_pol=init_pol/init_pol.sum(-1)[...,None]
+    end_pol = IPGA_stage_game(game,init_pol=init_pol)
+    greedy_pol_oh, greedy_r = game.solve_greedy_pol_batch(end_pol)
+    polnp = np.argmax(end_pol.detach().cpu().numpy(),1)
+    print("test finished")
+    #print(f"converged to a nash: {game.is_nash(polnp)}                     ")
+def test_dynamic():
+    import game_definition
+    game = game_definition.MultiStageGame(10,2,2,
+                                          mult_r_w=0,
+                                          controlable_t_w=1,
+                                          seed=1,device="cuda")
+    #init_pol = th.rand(game.n,game.ns, game.na,device=game.device,generator=game.gen)
+    init_pol = th.ones(game.n,game.ns, game.na,device=game.device)
+    init_pol=init_pol/init_pol.sum(-1)[...,None]
+    s_0 =1
+    end_pol,V = IPGA_dynamic_game_oracle(game,init_pol=init_pol,s_0=s_0)
+    print(f"Policy gradient V estimation: {V}                        ")
+    V_end_pol = game.play_game(end_pol)
+    print(f"Policy gradient V real:       {V_end_pol[s_0]}                        ")
+    m,argm = end_pol.max(-1)
+    greedy_pol = th.nn.functional.one_hot(argm,num_classes=game.na)
+    V_end_pol_greedy = game.play_game(greedy_pol)
+    print(f"Greedy Policy gradient V real:{V_end_pol_greedy[s_0]}                        ")
+    #polnp = np.argmax(end_pol.detach().cpu().numpy(),1)
+    opt_pol, opt_v = game.solve()
+    print(f"Policy iteration V:           {opt_v[s_0]}")
+    print(f"Number of wrong actions: {(opt_pol!=greedy_pol).sum()/2}")
+    pass
+if __name__ =="__main__":
+    test_stage()
---
+++ game_definition.py
@@ -0,0 +1,289 @@
+"""Implement static and dynamic games with random parameters
+   AUTHORS: Gabriel Vallat
+"""
+from typing import List, Tuple, Union
+import torch as th
+import torch.functional as F
+import numpy as np
+class StageGame():
+    def __init__(self,n_players,n_actions,coupling_w = 0, separable_type="additive", seed=None,device="cuda",aligned=True) -> None:
+        self.n = n_players
+        self.na = n_actions
+        self.seed = seed
+        self.device=device
+        self.gen = th.Generator(device=device)
+        if seed is not None:
+            self.gen.manual_seed(seed)
+        assert coupling_w <= 1, "The proportional weight should sum to 1"
+
+        self.r = th.zeros([self.na for i in range(self.n)],device=device)
+        """
+        self.r_prod_indiv = th.rand([self.n,self.na],generator=self.gen,device=device)
+        self.r_prod = th.ones([self.na]*self.n,device=device)
+        for i in range(self.n):
+            dim = [1]*i+[self.na]+[1]*(self.n-1-i)
+            self.r_prod*=self.r_prod_indiv[i].reshape(dim)
+        """
+        """    
+        self.r_prod_indiv = th.rand([self.n,self.na,self.na],generator=self.gen,device=device)
+        self.r_prod = self.r_prod_indiv.prod(0)""" 
+        """
+        #normalize the partial reward
+        self.r_prod-=self.r_prod.min()
+        self.r_prod/=self.r_prod.max()
+
+        self.r += self.r_prod*mult_pot_w
+        
+        self.r_add_indiv = th.rand(self.n,self.na,generator=self.gen,device=device)
+        self.r_add = self.r_add_indiv[0].reshape([self.na]+[1]*(self.n-1))
+        for p in range(1,self.n):
+            shape = np.ones(self.n,dtype=int)
+            shape[p]=self.na
+            self.r_add = self.r_add+self.r_add_indiv[p].reshape(tuple(shape))
+        
+        self.r_add = self.r_add
+        self.r_add-=self.r_add.min()
+        self.r_add/=self.r_add.max()
+        self.r+=self.r_add*(1-mult_pot_w)"""
+        self.r_sep_indiv = th.rand(self.n,self.na,generator=self.gen,device=device)
+        if separable_type == "additive":
+            self.r_sep = self.r_sep_indiv[0].reshape([self.na]+[1]*(self.n-1))
+            for p in range(1,self.n):
+                shape = np.ones(self.n,dtype=int)
+                shape[p]=self.na
+                self.r_sep = self.r_sep+self.r_sep_indiv[p].reshape(tuple(shape))
+        elif separable_type == "multiplicative":
+            self.r_sep = self.r_sep_indiv[0].reshape([self.na]+[1]*(self.n-1))
+            for p in range(1,self.n):
+                shape = np.ones(self.n,dtype=int)
+                shape[p]=self.na
+                self.r_sep = self.r_sep*self.r_sep_indiv[p].reshape(tuple(shape))
+        self.r_sep-=self.r_sep.min()
+        self.r_sep/=self.r_sep.max()
+
+        self.r_coupled = th.rand(self.n*[self.na],generator=self.gen,device=device)
+        self.r_coupled-=self.r_coupled.min()
+        self.r_coupled/=self.r_coupled.max()
+        if aligned:
+            argm_sep = np.unravel_index(th.argmax(self.r_sep).cpu(),self.r_sep.shape)
+            r_coupled_swap = self.r_coupled[argm_sep]
+            argm_coupled = np.unravel_index(th.argmax(self.r_coupled).cpu(),self.r_coupled.shape)
+            self.r_coupled[argm_coupled]=r_coupled_swap
+            self.r_coupled[argm_sep]=1
+            
+        self.r = self.r_coupled*coupling_w + self.r_sep*(1-coupling_w)
+        #normalize the total reward:
+        self.r-=self.r.min()
+        self.r/=self.r.max()
+    def play(self,actions: List[int])->int:
+        return self.r[tuple(actions)]
+    def solve_stoc_pol(self,stoc_pol):
+        marginal_reward = self.r
+        for p in range(self.n):
+            shape = np.ones(self.n-p,dtype=int)
+            shape[0] = self.na
+            marginal_reward = (stoc_pol[p].reshape(tuple(shape))*marginal_reward).sum(0)
+        return marginal_reward
+    def solve_stoc_pol_batch(self,stoc_pol):
+        marginal_reward = th.tile(self.r.unsqueeze(0),[stoc_pol.shape[0]]+self.n*[1])
+        for p in range(self.n):
+            shape = np.ones(1+self.n-p,dtype=int)
+            shape[0] = stoc_pol.shape[0]
+            shape[1] = self.na
+            marginal_reward = (stoc_pol[:,p].reshape(tuple(shape))*marginal_reward).sum(1)
+        return marginal_reward
+    def is_nash(self,deterministic_pol):
+        
+        for p in range(self.n):
+            pol_mp = list(deterministic_pol)
+            pol_mp[p]=slice(None)
+            if deterministic_pol[p]!=np.argmax(self.r.cpu().numpy()[tuple(pol_mp)]):
+                return False
+        return True
+    def solve_greedy_pol_batch(self,stoc_pol):
+        m,greedy_pol = stoc_pol.max(-1)
+        greedy_pol_oh = th.nn.functional.one_hot(greedy_pol,num_classes=self.na)
+        marginal_reward = th.tile(self.r.unsqueeze(0),[greedy_pol_oh.shape[0]]+self.n*[1])
+        for p in range(self.n):
+            shape = np.ones(1+self.n-p,dtype=int)
+            shape[0] = greedy_pol_oh.shape[0]
+            shape[1] = self.na
+            marginal_reward = (greedy_pol_oh[:,p].reshape(tuple(shape))*marginal_reward).sum(1)
+        return greedy_pol_oh,marginal_reward
+class AlignedStageGame(StageGame):
+    def __init__(self,n_players,n_actions,mult_pot_w = 0, seed=None,device="cuda"):
+        super.__init__(n_players,n_actions,mult_pot_w, seed,device)
+
+        #overwrite the reward
+        self.r = th.zeros([self.na for i in range(self.n)],device=device)
+        self.r_prod_indiv = th.rand([self.n]+[self.na for i in range(self.n)],generator=self.gen,device=device)
+        self.r_prod = self.r_prod_indiv.prod(0)
+        #normalize the partial reward
+        self.r_prod-=self.r_prod.min()
+        self.r_prod/=self.r_prod.max()
+
+        self.r += self.r_prod*mult_pot_w
+        
+        self.r_add_indiv = th.rand(self.n,self.na,generator=self.gen,device=device)
+        self.r_add = self.r_add_indiv[0].reshape([self.na]+[1]*(self.n-1))
+        for p in range(1,self.n):
+            shape = np.ones(self.n,dtype=int)
+            shape[p]=self.na
+            self.r_add = self.r_add+self.r_add_indiv[p].reshape(tuple(shape))
+        
+        self.r_add = self.r_add
+        self.r_add-=self.r_add.min()
+        self.r_add/=self.r_add.max()
+        self.r+=self.r_add*(1-mult_pot_w)
+        #normalize the total reward:
+        self.r-=self.r.min()
+        self.r/=self.r.max()
+class MultiStageGame():
+    def __init__(self,n_players,n_states,
+                 n_actions,
+                 mult_r_w=0,
+                 controlable_t_w=0,
+                 discount_factor = 0.1,
+                 seed=None,
+                 device='cuda') -> None:
+        self.n = n_players
+        self.na = n_actions
+        self.ns = n_states
+        self.seed = seed
+        self.random_np = np.random.default_rng(seed=seed)
+        self.device=device
+        self.gamma = 1-discount_factor
+        self.gen = th.Generator(device=device)
+        if seed is not None:
+            self.gen.manual_seed(seed)
+        assert mult_r_w <= 1, "The proportional weight of the cross terms in the reward function should be between 0 and 1"
+        assert controlable_t_w <= 1,"The proportional weight of the cross terms in the transition function should be between 0 and 1"
+        self.r = th.zeros([self.ns]+[self.na for i in range(self.n)],device=device)
+
+        self.r_prod_indiv = th.rand([self.n]+[self.ns]+[self.na for i in range(self.n)],generator=self.gen,device=device)
+        self.r_prod = self.r_prod_indiv.prod(0)
+        self.r_prod = self.r_prod/self.r_prod.max()*mult_r_w #normalize to keep the resulting reward between 0 and mult_pot_w
+        self.r += self.r_prod
+
+        self.r_add_indiv = th.rand(self.n,self.ns,self.na,generator=self.gen,device=device)
+        self.r_add = self.r_add_indiv[0].reshape([self.ns,self.na]+[1]*(self.n-1))
+        for p in range(1,self.n):
+            shape = np.ones(self.n+1,dtype=int)
+            shape[0]=self.ns
+            shape[p+1]=self.na
+            self.r_add = self.r_add+self.r_add_indiv[p].reshape(tuple(shape))
+        self.r_add = self.r_add/self.r_add.max()*(1-mult_r_w)
+        self.r += self.r_add
+
+        self.transition = th.zeros([self.ns]+[self.na for i in range(self.n)]+[self.ns],device = device)
+
+        self.t_uncontrolable = th.rand([self.ns,self.ns],generator=self.gen,device=device)
+        self.t_uncontrolable /= self.t_uncontrolable.sum(-1)[...,None]
+        #same transition function whatever the actions are
+        self.t_uncontrolable = th.tile(self.t_uncontrolable.reshape([self.ns]+[1 for i in range(self.n)]+[self.ns]),[1]+[self.na for i in range(self.n)]+[1])
+        self.transition+=self.t_uncontrolable*(1-controlable_t_w)
+
+        self.t_controlable = th.rand([self.ns]+[self.na for i in range(self.n)]+[self.ns],generator=self.gen,device=device)
+        self.t_controlable /= self.t_controlable.sum(-1)[...,None]
+           
+        self.transition+=self.t_controlable*controlable_t_w
+
+    def play_step(self,state,actions:List)->Tuple[int, int]:
+        reward = self.r[tuple([state]+list(actions))]
+        p = self.transition[tuple([state]+list(actions))].cpu().numpy()
+        next_state = self.random_np.choice(np.arange(self.ns),p=p)
+        return next_state,reward
+    def play_game_CCE(self,central_policy,s_0=None):
+        """Play the game using a central policy"""
+        trans_pol = self.transition.reshape(self.ns,-1,self.ns)
+        rew_pol = self.r.reshape(self.ns,-1)
+        trans_pol = (trans_pol*central_policy.unsqueeze(-1)).sum(1)
+        rew_pol = (rew_pol*central_policy).sum(1)
+        #rew_pol = rew_pol.sum(dim=tuple((range(1,self.na))))
+        #compute the discounted occupancy of each state
+        disc_occ = th.linalg.inv(th.diag(th.ones(self.ns,device=self.device))-self.gamma*trans_pol)
+
+        V_pol = disc_occ@rew_pol
+        if s_0 is not None:
+            return V_pol[s_0]
+        else:
+            return V_pol
+    def play_game(self,policies,s_0:Union[int,None]=None):
+        #first compute the induced Markov chain transition matrix and reward
+        trans_pol = self.transition
+        rew_pol = self.r
+        for i in range(self.n):
+            shape = np.ones(self.n+2-i,dtype=int)
+            shape[0]=self.ns
+            shape[1]=self.na
+            trans_pol = trans_pol*policies[i].reshape(tuple(shape))
+            trans_pol = trans_pol.sum(1)
+            rew_pol = rew_pol*policies[i].reshape(tuple(shape[:-1]))
+            rew_pol = rew_pol.sum(1)
+        #rew_pol = rew_pol.sum(dim=tuple((range(1,self.na))))
+        #compute the discounted occupancy of each state
+        disc_occ = th.linalg.inv(th.diag(th.ones(self.ns,device=self.device))-self.gamma*trans_pol)
+
+        V_pol = disc_occ@rew_pol
+        if s_0 is not None:
+            return V_pol[s_0]
+        else:
+            return V_pol
+    def solve(self):
+        """find the best deterministic policy"""
+        pol = th.randint(0,self.na,size=(self.n,self.ns),device=self.device)
+        while(True):
+            V_pol = self.play_game(th.nn.functional.one_hot(pol,num_classes=self.na))
+            Q_table = self.r+self.gamma*self.transition@V_pol
+            old_pol = pol
+            polidx = Q_table.reshape(self.ns,-1).argmax(axis=1)
+            pol = th.tensor(np.array(np.unravel_index(polidx.cpu(),[self.na]*self.n)),device=self.device)
+            if (old_pol==pol).all():
+                break
+        return th.nn.functional.one_hot(pol,num_classes=self.na),V_pol
+def test_stage():
+    game = StageGame(3,4,0,seed=3)
+    action1 = [0,2,1]
+    r1 = game.play(action1)
+    print(f"Reward for action {action1}: {r1}")
+    policy1 = th.zeros(game.n,game.na,device = game.device)
+    policy1[th.arange(game.n),action1]=1
+    e1 = game.solve_stoc_pol(policy1)
+    print(f"Expected reward for the deterministic policy {action1}: {e1}")
+    actions_stoc = np.tile(action1,(game.na,1))
+    actions_stoc[np.arange(game.na),0]=np.arange(game.na)
+    selected_player = 0
+    stoc_pol = th.tensor([0.2,0.3,0.4,0.1])
+    policy2 = policy1.clone()
+    policy2[selected_player]=stoc_pol
+    e2 = game.solve_stoc_pol(policy2)
+    e3 = game.solve_stoc_pol_batch(th.stack([policy1,policy2]))
+    a = action1
+    r2=0
+    for i in range(game.na):
+        a[selected_player]=i
+        r2+=stoc_pol[i]*game.play(a)
+    print(f"Test passed: {e1==r1 and abs(e2-r2)<1e-5}")
+def test_multi_stage():
+    game = MultiStageGame(3,10,5,
+                 mult_r_w=0,
+                 controlable_t_w=0,
+                 discount_factor = 0.1,
+                 seed=1)
+    policies = th.rand((game.n,game.ns,game.na),generator = game.gen,device = game.device)
+    policies = policies/policies.sum(-1)[...,None]
+    V_theoric = game.play_game(policies,s_0=0)
+    #game.solve()
+    V_emp = []
+    for test in range(10):
+        s = 0
+        V_sample =0
+        for i in range(10000):
+            actions = [game.random_np.choice(np.arange(game.na),p = policies[k,s].cpu().numpy()) for k in range(game.n)]
+            s,r = game.play_step(s,actions)
+            V_sample += np.power(game.gamma,i)*r
+        V_emp.append(V_sample.cpu().numpy())
+    print(f"V theoric: {V_theoric}\nV empiric: {np.mean(V_emp)}")
+if __name__ == "__main__":
+    print("Start test")
+    test_stage()
